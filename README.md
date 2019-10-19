287fcebfe1a7ff329c11e7c4a3fdf29b5b88846c
Mark Washeim <mark.washeim@newthinking.de>
2019-10-19 18:04:05 +0200

    Added a proper animation and changed the way tempo acts.

--- README.md
+++ README.md
@@ -5,7 +5,12 @@ The up and down keys change Frequency.
 
 The left and right keys change duration/tempo.
 
-Hold the B key to record changes in Frequency dynamically. There is a rough animation of the Freqs. Duration/Tempo and Freq (current). Are dispalyed.
+Hold the B key to record changes in Frequency dynamically. 
+Hold the A key to record changes in Tempo dynamically. 
+
+Each of 64 steps Freq and duration a played back or recorded. If you adjust the temp and continue holding A you'll get your tones with the new temp/duration. Glissandi are obvious.
+
+There is a rough animation of the Freqs. Duration/Tempo and Freq (current). Are dispalyed.
 
 The source is based on 
 http://hosted.hackaday.com/lunaMod45remix.pde
@@ -13,5 +18,5 @@ http://hosted.hackaday.com/lunaMod45remix.pde
 and the inspiration comes from:
 https://minimosynth.com/lunamod_generator/
 
-Theminimosynths are great. Buy one :) Or build one ;)
+The minimosynths are great. Buy one :) Or build one ;)
 
--- lunamidiboy.ino
+++ lunamidiboy.ino
@@ -23,8 +23,17 @@ int steps[] = {500, 500, 100, 100, 100, 100, 100, 100,             // this is ou
                500, 500, 100, 100, 100, 100, 100, 100,
                100, 100, 100, 100, 100, 100, 100, 100
               };
+int tempi[] = {20, 20, 20, 20, 20, 20, 20, 20,             // this is our time storage areae
+               20, 20, 20, 20, 20, 20, 20, 20,              
+               20, 20, 20, 20, 20, 20, 20, 20,              
+               20, 20, 20, 20, 20, 20, 20, 20,
+               20, 20, 20, 20, 20, 20, 20, 20,
+               20, 20, 20, 20, 20, 20, 20, 20,
+               20, 20, 20, 20, 20, 20, 20, 20,
+               20, 20, 20, 20, 20, 20, 20, 20,
+              };             
 
-int tempo = 10;                                      //tempo or speed between tones
+int tempo = 20;                                      //tempo or speed between tones
 int duration = 0;                                   //how long each of the 64 tones plays
 int frequency = 0;                                  //current tone
 int pitchval = 1;
@@ -93,12 +102,12 @@ void freqout(int freq, int t) {
 void buttonChecks() {
   if (arduboy.pressed(UP_BUTTON) )   {
     if (frequencyPot < 1023) {
-      frequencyPot += 7;
+      frequencyPot += 5;
     }
   }
   if (arduboy.pressed(DOWN_BUTTON) ) {
-    if (frequencyPot > 0) {
-      frequencyPot -= 7;
+    if (frequencyPot > 10) {
+      frequencyPot -= 5;
     }
   }
   if (arduboy.pressed(LEFT_BUTTON) ) {
@@ -107,10 +116,18 @@ void buttonChecks() {
     }
   }
   if (arduboy.pressed(RIGHT_BUTTON) ) {
-    if (tempo > 0) {
+    if (tempo > 10) {
       tempo -= 1;
     }
   }
+}
+
+void printCurVal () {
+      print_cur(F("Dur:     Freq:"));
+      sprintf(textBuf, "%d", tempo);
+      printText(textBuf, 34, 0, 1);
+      sprintf(textBuf, "%d", frequencyPot);
+      printText(textBuf, 90, 0, 1);
 }
 
 void loop() {
@@ -119,47 +136,50 @@ void loop() {
   if (!(arduboy.nextFrame()))
     return;
 
+
+  //clear screen for next run
   arduboy.clear();
-
-
-  print_cur(F("Dur:     Freq:"));
-  sprintf(textBuf, "%d", tempo);
-  printText(textBuf, 34, 0, 1);
-  sprintf(textBuf, "%d", frequencyPot);
-  printText(textBuf, 90, 0, 1);
-
+   
   for (int i = 0; i < 63; i++)                     //64 individual notes played
   {
-
+    
     currentStep = i;                                 //save our current position in the loop for later
-    int y = steps[i] % 64; 
-    arduboy.drawPixel( i*4 % 72, y , 1);
+    int y  = map(steps[i], 0, 1023, 64, 0);          // map values from frequency to the vertical axis
+    int x = (i + 1) * 2;
+    arduboy.drawPixel( x, y , 1);
+    arduboy.drawPixel(x+1,y, 1);
+    
 
     // checkset values from buttons.
     buttonChecks();
-
+    
+    // now print updated tempo and freq
+    printCurVal();
+    
+    arduboy.display();
+    
+    // modulating tempi :)
+    if ( arduboy.pressed(A_BUTTON) ){
+      tempi[currentStep] = tempo;
+    }
+    
     if ( arduboy.pressed(B_BUTTON) )               //is the program button being pressed
     { //if so lets write a new tone the this location
 
-      steps[currentStep] = (frequencyPot);  //read the frequency pot and set the new tone actual set up UP and Down
+      steps[currentStep] = (frequencyPot);              //write the freq after UP and Down
+      
       freqout (steps[currentStep], duration);           //set the parameters for frequout below and play it
       freqout (steps[currentStep] + 64, duration);     //play another tone a little bit different than the original to give
-      //it a little more depth. this can be changed to your liking
-      freqout (steps[currentStep] + 128, duration);     //play another tone a little bit different than the original to give
-      //it a little more depth. this can be changed to your liking
+      freqout (steps[currentStep] + 128, duration);     
     } else {                                            //else play the tone
 
       freqout (steps[currentStep], duration);           //set the parameters for frequout below and play it
       freqout (steps[currentStep] + 64, duration);     //play another tone a little bit different than the original to give
-      //it a little more depth. this can be changed to your liking
-      freqout (steps[currentStep] + 128, duration);     //play another tone a little bit different than the original to give
-      //it a little more depth. this can be changed to your liking
-
-
+      freqout (steps[currentStep] + 128, duration);     
     }
-    // tempo = (analogRead(tempoPot)/4);                    //read the tempo pot
-    duration = tempo / 8;                                 //set the individual tone durations
-    delay(tempo);                                         //wait a bit
+
+    duration = tempi[currentStep] / 4;                     //set the individual tone durations
+    delay(tempi[currentStep]);                                         //wait a bit
   }
-  arduboy.display();
+  
 }
